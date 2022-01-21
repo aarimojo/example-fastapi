84ae75d624433830762f9f9e5c44fd43b99725fd
aari <aarimojo@gmail.com>
2022-01-20 22:43:58 -0500

    try catch in db

--- app/database.py
+++ app/database.py
@@ -7,30 +7,35 @@ from psycopg2.extras import RealDictCursor
 from .config import settings
 
 SQLALCHEMY_URL = f'{settings.database_url_real}'
+print(SQLALCHEMY_URL)
 
-engine = create_engine(SQLALCHEMY_URL)
+try:
+    engine = create_engine(SQLALCHEMY_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+    Base = declarative_base()
 
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='aaripostgres', 
-        password='Zapopan10', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection successful')
-        break
-    except Exception as error:
-        print('Connection to database failed')
-        print('Error: ', error)
-        time.sleep(2)
+    while True:
+        try:
+            conn = psycopg2.connect(host='localhost', database='fastapi', user='aaripostgres', 
+            password='Zapopan10', cursor_factory=RealDictCursor)
+            cursor = conn.cursor()
+            print('Database connection successful')
+            break
+        except Exception as error:
+            print('Connection to database failed')
+            print('Error: ', error)
+            time.sleep(2)
+
+except:
+    print('shits fucked')
