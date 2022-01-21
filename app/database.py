5e279f8c2d7f2047e00a43f4042f370701bd4fe0
aari <aarimojo@gmail.com>
2022-01-20 21:39:54 -0500

    First commit

---
+++ app/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+from time import time
+from psycopg2.extras import RealDictCursor
+from .config import settings
+
+SQLALCHEMY_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}\
+                    @{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
+engine = create_engine(SQLALCHEMY_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='aaripostgres', 
+        password='Zapopan10', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Database connection successful')
+        break
+    except Exception as error:
+        print('Connection to database failed')
+        print('Error: ', error)
+        time.sleep(2)
